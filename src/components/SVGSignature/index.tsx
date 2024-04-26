a769877208dfd4ce73f893f511ac328e677e9706
QuentinHsu <xuquentinyang@gmail.com>
2024-04-26 14:55:58 +0800

    perf: PostDetailLayout

--- src/components/SVGSignature/index.tsx
+++ src/components/SVGSignature/index.tsx
@@ -4,8 +4,8 @@ import './index.scss'
 function SVGSignature() {
   const strokeColor = 'var(--gray-0)'
   return (
-    <div className="signature">
-      <svg viewBox="0 0 550 110" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg">
+    <div className="signature" style={{ width: '100%', height: '100%' }}>
+      <svg style={{ width: '100%', height: '100%' }} viewBox="0 0 550 110" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg">
         <path
           d={SVGSignaturePath}
           stroke={strokeColor}
